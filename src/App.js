b7502a14a4de10b158f8e856174381f59bcf6fc5
Jacob Jenkins <me@jacobejenkins.com>
2019-03-25 22:36:02 -0400

    New styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
-import scrollIntoView from 'scroll-into-view'
+import React, { Component } from "react"
+import styled, { keyframes } from "styled-components"
+import scrollIntoView from "scroll-into-view"
 
-const importAll = (r) => {
-  return r.keys().map(r);
+const importAll = r => {
+  return r.keys().map(r)
 }
 
 const Container = styled.div`
@@ -16,16 +16,19 @@ const Container = styled.div`
 const Post = styled.div`
   max-width: 500px;
   width: 100%;
-  border: solid 1px #dcdcdc;
+  border: solid 1px #aaaaaa;
   margin-bottom: 20px;
   border-radius: 3px;
 `
 
 const Header = styled.div`
-  font-family: courier;
-  font-weight: bold;
+  font-family: input-sans, sans-serif;
+  font-weight: 400;
+  line-height: 0.90;
   font-size: 16px;
-  border-bottom: solid 1px #dcdcdc;
+  border-bottom: solid 1px #aaaaaa;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
   padding: 15px;
   background: white;
 `
@@ -56,55 +59,45 @@ const HeadingContainer = styled.div`
 `
 
 const Heading = styled.div`
-  font-family: "cooper-black-std";
-  font-size: 120px;
-  @media(max-width: 768px) {
+  font-family: input-sans, sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  margin: 0 -2px;
+  font-style: normal;
+  font-size: 140px;
+  @media (max-width: 768px) {
     font-size: 50px;
   }
 `
 
 const wave = keyframes`
   0% {
-    transform: translateY(90px); 
+    transform: translateY(30px) scale(1.0);
   }
   100% {
-    transform: translateY(-90px);
+    transform: translateY(-30px) scale(1.1);
   }
 `
 
 const HeadingSpan = styled.span`
-  color: red;
+  color: #0000ff;
   display: inline-block;
   animation-name: ${wave};
   animation-duration: 0.5s;
   animation-delay: -${props => props.index / 50}s;
   animation-iteration-count: infinite;
   animation-direction: alternate;
-  text-shadow:
-  0 1px #900000,
-  1px 0 #880000,
-  1px 2px #800000,
-  2px 1px #780000,
-  2px 3px #700000,
-  3px 2px #680000,
-  3px 4px #600000,
-  4px 3px #580000,
-  4px 5px #500000,
-  5px 4px #480000,
-  5px 6px #400000,
-  6px 5px #380000,
-  6px 7px #300000,
-  7px 6px #280000,
-  7px 8px #200000,
-  8px 7px #180000,
-  8px 9px #100000,
-  9px 8px #080000,
-  9px 10px #000000;
+  text-shadow: 0 1px #0000ff, 1px 0 #0000ee, 1px 2px #0000dd, 2px 1px #0000cc,
+    2px 3px #0000bb, 3px 2px #0000aa, 3px 4px #000099, 4px 3px #000088,
+    4px 5px #000077, 5px 4px #000066, 5px 6px #000055, 6px 5px #000044,
+    6px 7px #000033, 7px 6px #000022, 7px 8px #000011, 8px 7px #000000,
+    8px 9px #000000, 9px 8px #000000, 9px 10px #000000;
 `
 
 const Button = styled.button`
-  font-family: courier;
-  font-weight: bold;
+  font-family: input-sans, sans-serif;
+
+  font-weight: 400;
   font-size: 16px;
   background-color: #dcdcdc;
   border: solid 1px #dcdcdc;
@@ -124,20 +117,21 @@ const PlayButton = Button.extend`
 `
 
 Array.prototype.shuffle = function() {
-  var input = this;
-   
-  for (var i = input.length-1; i >=0; i--) {
-   
-      var randomIndex = Math.floor(Math.random()*(i+1)); 
-      var itemAtIndex = input[randomIndex]; 
-       
-      input[randomIndex] = input[i]; 
-      input[i] = itemAtIndex;
+  var input = this
+
+  for (var i = input.length - 1; i >= 0; i--) {
+    var randomIndex = Math.floor(Math.random() * (i + 1))
+    var itemAtIndex = input[randomIndex]
+
+    input[randomIndex] = input[i]
+    input[i] = itemAtIndex
   }
-  return input;
+  return input
 }
 
-const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg|mp4)$/))
+const images = importAll(
+  require.context("./images", false, /\.(png|jpe?g|svg|mp4)$/)
+)
 
 // Initial shuffle
 images.shuffle()
@@ -148,54 +142,66 @@ const text2 = "Reported"
 class App extends Component {
   constructor() {
     super()
-    this.state = {clicked: false}
+    this.state = { clicked: false }
   }
 
   onClick = () => {
     if (!this.state.clicked) {
-      this.setState({clicked: true})
-      const posts = document.getElementsByClassName('post')
+      this.setState({ clicked: true })
+      const posts = document.getElementsByClassName("post")
       for (let i = 0; i < posts.length; i++) {
         setTimeout(() => {
           scrollIntoView(posts[i])
-        }, 15000 + (i * 15000))
+        }, 15000 + i * 15000)
       }
 
       setTimeout(() => {
         document.documentElement.scrollTop = 0
         images.shuffle()
-        this.setState({clicked: false})
-        this.onClick() 
-      }, 20000 + (posts.length * 15000))      
+        this.setState({ clicked: false })
+        this.onClick()
+      }, 20000 + posts.length * 15000)
     }
   }
 
   render() {
     return (
       <Container>
-        <PlayButton disabled={this.state.clicked} onClick={this.onClick}>Play</PlayButton>
+        <PlayButton disabled={this.state.clicked} onClick={this.onClick}>
+          Scroll
+        </PlayButton>
         <HeadingContainer>
           <Heading>
-            {text1.split("").map((t, i) => <HeadingSpan index={i} key={i}>{t === " " ? '\u00A0' : t}</HeadingSpan>)}
+            {text1.split("").map((t, i) => (
+              <HeadingSpan index={i} key={i}>
+                {t === " " ? "\u00A0" : t}
+              </HeadingSpan>
+            ))}
           </Heading>
           <Heading>
-            {text2.split("").map((t, i) => <HeadingSpan index={i} key={i}>{t === " " ? '\u00A0' : t}</HeadingSpan>)}
+            {text2.split("").map((t, i) => (
+              <HeadingSpan index={i} key={i}>
+                {t === " " ? "\u00A0" : t}
+              </HeadingSpan>
+            ))}
           </Heading>
-
         </HeadingContainer>
-        {images.map(i => 
+        {images.map(i => (
           <Post className="post">
-            <Header>@{i.replace(/^.*[\\\/]/, '').split(' - ')[0]}</Header>
-            {i.split('.')[2] === "mp4" ? 
-            <Video autoPlay loop muted>
-              <source src={i} type="video/mp4" />
-            </Video> : <Image src={i} />}
-            <Footer><Button>BLOCK</Button><Button>REPORT</Button></Footer>
+            <Header>@{i.replace(/^.*[\\\/]/, "").split(" - ")[0]}</Header>
+            {i.split(".")[2] === "mp4" ? (
+              <Video autoPlay loop muted>
+                <source src={i} type="video/mp4" />
+              </Video>
+            ) : (
+              <Image src={i} />
+            )}
+            {<Footer><Button>Block</Button><Button>Report</Button></Footer>}
           </Post>
-        )}
+        ))}
       </Container>
     )
   }
 }
 
-export default App;
+export default App
